1651d72a604043242df3426c133510568beb5e13
Theo Hernandez <h_theo2001@hotmail.com>
2024-11-07 17:48:26 +0100

    dec secret and check when uploading a file

--- backend/Extensions/Upload.cs
+++ backend/Extensions/Upload.cs
@@ -5,7 +5,17 @@ using System.Diagnostics;
 
 namespace backend.Extensions;
 
+
+
 public static class UploadExtensions{
+
+    public static Random random = new Random();
+
+    public static string RandomFileId(int length){
+        const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return new string (Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
     public static WebApplication UploadRestApi(this WebApplication app){
         
         app.MapPost("/upload", async (Upload newUpload, NpgsqlConnection connection)=>{
@@ -13,10 +23,32 @@ public static class UploadExtensions{
 
             await connection.OpenAsync();
 
-            var query = $"INSERT INTO File (fileid, username, path, size, date, name, type) VALUES (@fileid, @username, @path, @size, @date, @name, @type); INSERT INTO Upload (username, fileid, date, maxd, secret) VALUES (@username, @fileid, @date, @maxd, @secret)";
+            int check = -1;
+
+            var maxUploadCheckQuery = "SELECT maxupload FROM Secret WHERE secret = @secret;";
+
+            using (var cmd = new NpgsqlCommand(maxUploadCheckQuery, connection)){
+                cmd.Parameters.AddWithValue("secret", newUpload.secret);
+
+                using (var reader = await cmd.ExecuteReaderAsync()){
+                    while(await reader.ReadAsync()){
+                        check = reader.GetInt32(reader.GetOrdinal("maxupload"));
+                    }
+                }                
+            }
+
+            if (check < 1 ){
+                return Results.Unauthorized();
+            }
+
+            var query1 = "INSERT INTO File (fileid, username, path, size, date, name, type) VALUES (@fileid, @username, @path, @size, @date, @name, @type);";
+            var query2 = "INSERT INTO Upload (username, fileid, date, maxd, secret) VALUES (@username, @fileid, @date, @maxd, @secret);";
+            var query3 = "UPDATE Secret SET maxupload = maxupload -1;";
+
+            var query = $"{query1} {query2} {query3}";
             DateTime d = new DateTime(2024,01,01,0,0,0,DateTimeKind.Utc);
             using (var cmd = new NpgsqlCommand(query, connection)){
-                cmd.Parameters.AddWithValue("fileid", "aabbccdd");
+                cmd.Parameters.AddWithValue("fileid", RandomFileId(20));
                 cmd.Parameters.AddWithValue("username", newUpload.username);
                 cmd.Parameters.AddWithValue("path", "/tmp/lol");
                 cmd.Parameters.AddWithValue("size", newUpload.size);
